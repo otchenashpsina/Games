a13f590dfc0509885679cabfd45c2a3202216438
otchenashpsina <49341776+otchenashpsina@users.noreply.github.com>
2021-09-05 19:55:50 +0300

    Add files via upload

---
+++ Shoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+    public Transform arrowLocation;
+    public float shotPower;
+    public AudioSource shootSound;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(arrowPrefab, arrowLocation.position, arrowLocation.rotation).GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * shotPower);
+            shootSound.Play();
+        }
+
+    }
+}
